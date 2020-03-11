9b9c49e52efdc0eb8a9fdfcbecc7a99a138b1d8b
malith97 <reddogmalith@gmail.com>
2020-03-11 07:45:21 +0530

    increment in drawing page of childrens book

--- final year 1/Assets/scripts/children book scripts/Applycolor.cs
+++ final year 1/Assets/scripts/children book scripts/Applycolor.cs
@@ -5,15 +5,50 @@ using UnityEngine;
 public class Applycolor : MonoBehaviour
 {
     public FlexibleColorPicker fcp;
-    public Material material;
-    void Start()
+    public Material[] material;
+
+    public Color externalColor;
+    private Color internalColor;
+
+    private void Start()
     {
-        material.color = fcp.color;
+        internalColor = externalColor;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //apply color of this script to the FCP whenever it is changed by the user
+        if (internalColor != externalColor)
+        {
+            fcp.color = externalColor;
+            internalColor = externalColor;
+        }
+
+        RaycastHit hitInfo = new RaycastHit();
+        bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+
+        if (hitInfo.transform.gameObject.tag == "treewithoutleaves")
+        {
+            //extract color from the FCP and apply it to the object material
+            material[0].color = fcp.color;
+        }
+
+        if (hitInfo.transform.gameObject.tag == "leaves")
+        {
+            
+            material[1].color = fcp.color;
+        }
+        if (hitInfo.transform.gameObject.tag == "grass")
+        {
+            
+            material[2].color = fcp.color;
+        }
+        if (hitInfo.transform.gameObject.tag == "rock")
+        {
+
+            material[3].color = fcp.color;
+        }
+
+
     }
 }
--- final year 1/Assets/scripts/children book scripts/SHAPECHANGECONTROL.cs
+++ final year 1/Assets/scripts/children book scripts/SHAPECHANGECONTROL.cs
@@ -5,11 +5,37 @@ using Lean.Touch;
 using UnityEngine.UI;
 public class SHAPECHANGECONTROL : MonoBehaviour
 {
-    public GameObject object1;
+    int a;
+    public GameObject[] objects;
     private LeanDragDeformMesh treewithoutbranches;
+    private LeanDragDeformMesh leaves;
+    private LeanDragDeformMesh grass;
+    private LeanDragDeformMesh rock;
+    //private Movescript[] movearr;
+    private LeanDragTranslate leavemove;
+    private Movescript movearr1;
+    private Movescript movearr2;
+    private Movescript movearr3;
     void Start()
     {
-        treewithoutbranches = object1.GetComponent<LeanDragDeformMesh>();
+        a = 0;
+        treewithoutbranches = objects[0].GetComponent<LeanDragDeformMesh>();
+        leaves = objects[1].GetComponent<LeanDragDeformMesh>();
+        grass = objects[2].GetComponent<LeanDragDeformMesh>();
+        rock = objects[3].GetComponent<LeanDragDeformMesh>();
+        treewithoutbranches.enabled = false;
+        leaves.enabled = false;
+        grass.enabled = false;
+
+        // movearr[0]= objects[0].GetComponent<Movescript>();
+        //movearr[1] = objects[1].GetComponent<Movescript>();
+        //movearr[2] = objects[2].GetComponent<Movescript>();
+        movearr1 = objects[0].GetComponent<Movescript>();
+        movearr2 = objects[2].GetComponent<Movescript>();
+        movearr3 = objects[3].GetComponent<Movescript>();
+        leavemove = objects[1].GetComponent<LeanDragTranslate>();
+
+
     }
 
     // Update is called once per frame
@@ -27,8 +53,43 @@ public class SHAPECHANGECONTROL : MonoBehaviour
         //{
 
         //}
+        a += 1;
+        if (a == 1)
+        {
+            
+            treewithoutbranches.enabled = true;
+            leaves.enabled = true;
+            grass.enabled = true;
 
-        treewithoutbranches.enabled = false;
+            //movearr[0].enabled = false;
+            //movearr[1].enabled = false;
+            //movearr[2].enabled = false;
+            movearr1.enabled = false;
+            movearr2.enabled = false;
+            movearr3.enabled = false;
+            leavemove.enabled = false;
+
+        }
+        if (a == 2)
+        {
+            treewithoutbranches.enabled = false;
+            leaves.enabled = false;
+            grass.enabled = false;
+
+            //movearr[0].enabled = true;
+            //movearr[1].enabled = true;
+            //movearr[2].enabled = true;
+
+            movearr1.enabled = true;
+            movearr2.enabled = true;
+            movearr3.enabled = true;
+            leavemove.enabled = true;
+
+            a = 0;
+        }
+
+
+
 
     }
 
